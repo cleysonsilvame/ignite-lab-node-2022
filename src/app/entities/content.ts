3522ae9e6930a39a8592daefda3f75c2f8c344a9
Cleyson Silva <cleysonsilva.me@gmail.com>
2022-12-17 08:00:38 +0000

    day 2

---
+++ src/app/entities/content.ts
@@ -0,0 +1,19 @@
+export class Content {
+  private readonly content: string;
+
+  constructor(content: string) {
+    const isContentLengthValid = this.validateContentLength(content);
+
+    if (!isContentLengthValid) throw new Error('Content length error.');
+
+    this.content = content;
+  }
+
+  get value() {
+    return this.content;
+  }
+
+  private validateContentLength(content: string) {
+    return content.length >= 5 && content.length <= 240;
+  }
+}
